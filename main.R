dffbf11da136b757f9d27986b4be4d21260c06e0
agouy <contact@agouy.com>
2021-01-08 22:22:01 +0000

    first commit

--- main.R
+++ main.R
@@ -2,8 +2,13 @@ library(tercen)
 library(dplyr)
 
 (ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+  select(.y, .ci, .ri) %>%
+  count(.ri, .ci) %>%
+  group_by(.ri) %>%
+  mutate(prop_per_row = n / sum(n)) %>%
+  group_by(.ci) %>%
+  mutate(prop_per_col = n / sum(n)) %>%
+  ungroup() %>%
+  mutate(prop_overall = n / sum(n)) %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,15 +1,20 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "01cb95cd7b746443ed9f40625200ef4f")
+options("tercen.stepId"     = "635b15c9-55d2-466d-bda9-6e9469b67532")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
 (ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+  select(.y, .ci, .ri) %>%
+  count(.ri, .ci) %>%
+  group_by(.ri) %>%
+  mutate(prop_per_row = n / sum(n)) %>%
+  group_by(.ci) %>%
+  mutate(prop_per_col = n / sum(n)) %>%
+  ungroup() %>%
+  mutate(prop_overall = n / sum(n)) %>%
   ctx$addNamespace() %>%
   ctx$save()
